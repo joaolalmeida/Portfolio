af42287a47780b2918e77cdd05ba9ac2e3c04dc9
joaolalmeida <joaolalmeida20@gmail.com>
2023-04-03 18:21:16 -0300

    adicionando novo projeto

--- src/pages/projetos/Projetos.jsx
+++ src/pages/projetos/Projetos.jsx
@@ -1,11 +1,15 @@
 import { HeaderComplete } from './../../components/HeaderPast/Header'
 import { Footer } from './../../components/Footer/Footer'
 import { Project } from '../../components/main/Project'
+
 import landingPage from './../../assets/landingPage.png'
 import rocketPlay from './../../assets/rocketPlay.png'
+import portfolio from "./../../assets/portfolio.png"
+
 import linhaHorizontal from './../../assets/linha-horizontal.png'
 
 import styles from './Projetos.module.css'
+
 const landingPageTexto = [
   "HTML",
   "CSS",
@@ -17,6 +21,16 @@ const rocketPlayTexto = [
   "JavaScript",
   "Json",
 ]
+const portfolioTexto = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "ReactJS",
+  "CSS Modules",
+  "React Router DOM",
+  "Framer Motion",
+]
+
 export function Projetos() {
   return (
     <div>
@@ -24,7 +38,6 @@ export function Projetos() {
 
       <Project
         texto="Landing Page Responsive"
-        texto1="teste"
         link={"https://responsive-landing-page-joaolalmeida.netlify.app/"}
         imageSrc={landingPage}
         imageLinha={linhaHorizontal}
@@ -42,11 +55,19 @@ export function Projetos() {
         github={"https://github.com/joaolalmeida/Rocketpay"}
       />
 
+      <Project
+        texto="Portfólio"
+        link={"https://portfolio-joaoalmeida.netlify.app/"}
+        imageSrc={portfolio}
+        textos={portfolioTexto}
+        href={"https://portfolio-joaoalmeida.netlify.app/"}
+        github={"https://github.com/joaolalmeida/Portifolio"}
+      />
+
+      <div className={styles.footerProjetos}></div>
       <div className={styles.footerProjetos}>
+        <Footer />
       </div>
-        <div className={styles.footerProjetos}>
-          <Footer />
-        </div>
     </div>
   )
 }
